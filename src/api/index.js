bfe9ff4b997ed6e1c52772409737577c94b1026d
Mohd Maaz <97888220+mohdmaaz2000@users.noreply.github.com>
2023-07-20 11:41:00 +0530

    Fiexed some responsive issues

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.REACT_APP_SERVER,
     timeout: 1000 * 30,
     validateStatus: (status) => {
         return status >= 200 && status <= 505
